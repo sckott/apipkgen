1abc2b1323a17880cbe32ef9357207afe3447e4e
Scott Chamberlain <sachamber@fredhutch.org>
2024-08-14 16:42:29 -0700

    replace most of create_pkg with usethis fxns #21

--- R/apipkgen-package.R
+++ R/apipkgen-package.R
@@ -3,5 +3,11 @@
 
 ## usethis namespace: start
 #' @import yaml
+#' @importFrom usethis create_package
+#' @importFrom usethis use_import_from
+#' @importFrom usethis use_mit_license
+#' @importFrom usethis use_package
+#' @importFrom usethis use_package_doc
+#' @importFrom withr with_dir
 ## usethis namespace: end
 NULL
